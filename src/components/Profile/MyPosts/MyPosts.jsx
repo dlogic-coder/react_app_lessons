a7dd08aa7c82fd41d6a0e3a86d39dfeb7af25b4c
avcom <avcom@ukr.net>
2020-09-28 23:27:02 +0300

    Lessons 16 : Add the props - as very impotant theme in React

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,7 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post />
+                <Post message="Hi, how are you?" likes="15" />
+                <Post message="It's my first post" likes="20" />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/Avatar_2_logo.jpg/220px-Avatar_2_logo.jpg" alt=""/>
-            post 1
+            {props.message}
+            <div>
+                <span>like: {props.likes}</span>
+            </div>
         </div>
     );
 }
