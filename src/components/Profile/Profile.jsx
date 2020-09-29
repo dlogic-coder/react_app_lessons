69406e61a2a21dfd0662921b4b131eced3006f81
avcom <avcom@ukr.net>
2020-09-29 18:25:51 +0300

    Lesson 18 : Created the Dialogs section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='http://localhost:3000/human-brain.jpg' />
             </div>
