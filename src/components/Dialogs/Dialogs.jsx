62980f80135812ea1ea05c4819827ea130a8818b
avcom <avcom@ukr.net>
2020-09-30 20:58:12 +0300

    Lesson 24 : UI-BLL - take the data aside

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,20 +17,32 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+let dialogsData = [
+   {id: 1, name: 'Dimych'},
+   {id: 2, name: 'Andrey'},
+   {id: 3, name: 'Sveta'},
+   {id: 4, name: 'Sasha'},
+   {id: 5, name: 'Viktor'},
+   {id: 6, name: 'Valera'}
+]
+
+let messagesData = [
+   {id: 1, message: 'Hi'},
+   {id: 2, message: 'How are you?'},
+   {id: 3, message: 'Yo'},
+   {id: 4, message: 'Yo'}
+]
+
    return (
       <div className={s.dialogs}>
          <div className={s.dialogs__items}>
-            <DialogItem id='1' name='Dimych' />
-            <DialogItem id='2' name='Andrey' />
-            <DialogItem id='3' name='Sveta' />
-            <DialogItem id='4' name='Sasha' />
-            <DialogItem id='5' name='Viktor' />
-            <DialogItem id='6' name='Valera' />
+            <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
+            <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
          </div>
          <div className={s.messages}>
-            <Message message='Hi' />
-            <Message message='How are you?' />
-            <Message message='Yo' />
+            <Message message={messagesData[0].message} />
+            <Message message={messagesData[1].message} />
          </div>
       </div>
    )
