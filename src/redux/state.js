d35cbaad88b8b575572ec3493a8bb0f2ec4fc70c
avcom <avcom@ukr.net>
2020-10-09 16:40:49 +0300

    Lesson 37 : state -> store, OOP, refactoring

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,52 +1,63 @@
-let rerenderEntireTree = () => { }
+let store = {
 
-let state = {
-   profilePage: {
-      posts: [
-         { id: 1, message: 'Hi, how are you?', likesCount: 15 },
-         { id: 2, message: 'It\'s my first post', likesCount: 20 }
-      ],
-      newPostText: 'DLogic'
+   _state: {
+      profilePage: {
+         posts: [
+            { id: 1, message: 'Hi, how are you?', likesCount: 15 },
+            { id: 2, message: 'It\'s my first post', likesCount: 20 }
+         ],
+         newPostText: 'DLogic'
+      },
+
+      dialogsPage: {
+         dialogs: [
+            { id: 1, name: 'Dimych' },
+            { id: 2, name: 'Andrey' },
+            { id: 3, name: 'Sveta' },
+            { id: 4, name: 'Sasha' },
+            { id: 5, name: 'Viktor' },
+            { id: 6, name: 'Valera' }
+         ],
+         messages: [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How are you?' },
+            { id: 3, message: 'Yo' },
+            { id: 4, message: 'Yo' }
+         ]
+      }
    },
 
-   dialogsPage: {
-      dialogs: [
-         { id: 1, name: 'Dimych' },
-         { id: 2, name: 'Andrey' },
-         { id: 3, name: 'Sveta' },
-         { id: 4, name: 'Sasha' },
-         { id: 5, name: 'Viktor' },
-         { id: 6, name: 'Valera' }
-      ],
-      messages: [
-         { id: 1, message: 'Hi' },
-         { id: 2, message: 'How are you?' },
-         { id: 3, message: 'Yo' },
-         { id: 4, message: 'Yo' }
-      ]
+   getState() {
+      return this._state;
+   },
+
+   _callSubscriber() { },
+
+   addPost() {
+      let newPost = {
+         id: 5,
+         message: this._state.profilePage.newPostText,
+         likesCount: 0
+      };
+
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = '';
+      this._callSubscriber(this._state);
+   },
+
+   updateNewPostText(newText) {
+      this._state.profilePage.newPostText = newText;
+      this._callSubscriber(this._state);
+   },
+
+   subscribe(observer) {
+      this._callSubscriber = observer;
    }
 }
 
-export let addPost = () => {
-   let newPost = {
-      id: 5,
-      message: state.profilePage.newPostText,
-      likesCount: 0
-   };
 
-   state.profilePage.posts.push(newPost);
-   state.profilePage.newPostText = '';
-   rerenderEntireTree(state);
-}
+export default store;
 
-export let updateNewPostText = (newText) => {
-   state.profilePage.newPostText = newText;
-   rerenderEntireTree(state);
-}
+//window.store = store;
 
-// This is pattern "Observer"
-export let subscribe = (observer) => {
-   rerenderEntireTree = observer;
-}
-
-export default state;
+// store - OOP
