1cf6bb062487db98f6dbcdecce0794fa8f2623d3
avcom <avcom@ukr.net>
2020-10-13 15:33:10 +0300

    Lesson 40 : add Message into Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 import s from './Dialogs.module.css';
+import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/state';
 
 const Dialogs = (props) => {
+   let state = props.store.getState().dialogsPage;
 
-   let dialogsElements  = props.state.dialogs.map( dialog => <DialogItem id={dialog.id} name={dialog.name} /> );
-   let messagesElements = props.state.messages.map( msg => <Message message={msg.message} /> );
+   let dialogsElements = state.dialogs.map(dialog => <DialogItem id={dialog.id} name={dialog.name} />);
+   let messagesElements = state.messages.map(msg => <Message message={msg.message} />);
+   
+   let newMessageBody = state.newMessageBody;
+
+   let onSendMessageClick = () => {
+      props.store.dispatch(sendMessageCreator());
+   }
+
+   let onNewMessageChange = (e) => {
+      let body = e.target.value;
+      props.store.dispatch(updateNewMessageBodyCreator(body));
+   }
 
    return (
       <div className={s.dialogs}>
@@ -14,7 +27,13 @@ const Dialogs = (props) => {
             {dialogsElements}
          </div>
          <div className={s.messages}>
-            {messagesElements}
+            <div>{messagesElements}</div>
+            <div>
+               <div><textarea value={newMessageBody}
+                  onChange={onNewMessageChange}
+                  placeholder="Enter your message"></textarea></div>
+               <div><button onClick={onSendMessageClick}>Send</button></div>
+            </div>
          </div>
       </div>
    )
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,9 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
+
 let store = {
 
    _state: {
@@ -26,8 +29,11 @@ let store = {
             { id: 2, message: 'How are you?' },
             { id: 3, message: 'Yo' },
             { id: 4, message: 'Yo' }
-         ]
-      }
+         ],
+         newMessageBody: ""
+      },
+
+      sidebar: {}
    },
 
    _callSubscriber() { },
@@ -72,20 +78,33 @@ let store = {
       else if (action.type === UPDATE_NEW_POST_TEXT) {
          this._state.profilePage.newPostText = action.newText;
          this._callSubscriber(this._state);
+      } 
+      else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+         this._state.dialogsPage.newMessageBody = action.body;
+         this._callSubscriber(this._state);
+      } 
+      else if (action.type === SEND_MESSAGE) {
+         let body = this._state.dialogsPage.newMessageBody;
+         this._state.dialogsPage.newMessageBody = '';
+         this._state.dialogsPage.messages.push({id: 6, message: body});
+         this._callSubscriber(this._state);
       }
    }
 }
 
-export const addPostActionCreator = () => {
-   return {
-       type: ADD_POST
-   }
-}
-
+export const addPostActionCreator = () => ({type: ADD_POST})
 export const updateNewPostTextActionCreator = (text) => {
    return {
        type: UPDATE_NEW_POST_TEXT, 
        newText: text
+   }
+}
+
+export const sendMessageCreator = () => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyCreator = (text) => {
+   return {
+       type: UPDATE_NEW_MESSAGE_BODY,
+       body: text
    }
 }
 
