c04fa5621811761efebb305fd26162f1207c0bde
avcom <avcom@ukr.net>
2020-10-23 21:36:38 +0300

    Lesson 50-51 : REST API, Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,16 @@
 import React from "react";
 import styles from './Users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/img/no_user.png';
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([{
-            id: 1,
-            photoUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/Avatar_2_logo.jpg/220px-Avatar_2_logo.jpg',
-            followed: false,
-            fullName: 'Andrew T.',
-            status: 'I am a boss',
-            location: {city: 'Kyiv', country: 'Ukraine'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/Avatar_2_logo.jpg/220px-Avatar_2_logo.jpg',
-                followed: false,
-                fullName: 'Dmytro K.',
-                status: 'SEO',
-                location: {city: 'Kharkiv', country: 'Ukraine'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/Avatar_2_logo.jpg/220px-Avatar_2_logo.jpg',
-                followed: true,
-                fullName: 'Maxim K.',
-                status: 'Frontend',
-                location: {city: 'Odessa', country: 'Ukraine'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/8b/Avatar_2_logo.jpg/220px-Avatar_2_logo.jpg',
-                followed: true,
-                fullName: 'Vasyl B.',
-                status: 'Backend',
-                location: {city: 'Lviv', country: 'Ukraine'}
-            }]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+            props.setUsers(response.data.items);
+            }
+        );
     }
 
 
@@ -45,21 +19,22 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
-                        {u.followed ? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                        {u.followed
+                            ? <button onClick={ () => { props.unfollow(u.id) }}>Unfollow</button>
                             : <button onClick={ () => { props.follow(u.id) }}>Follow</button>}
                     </div>
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.city}</div>
-                        <div>{u.location.country}</div>
+                        <div>{"u.location.city"}</div>
+                        <div>{"u.location.country"}</div>
                     </span>
                 </span>
             </div>)
